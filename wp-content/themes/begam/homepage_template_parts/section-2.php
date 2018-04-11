b07111d89268939efd52bd8f94c866c866969697
Garrett Cullen <garrettwcullen@gmail.com>
2018-04-11 16:22:01 -0700

    push

--- wp-content/themes/begam/homepage_template_parts/section-2.php
+++ wp-content/themes/begam/homepage_template_parts/section-2.php
@@ -15,8 +15,16 @@
 				<?php echo file_get_contents("wp-content/themes/begam/images/new_pattern.svg"); ?>
 				
 				<span class="sec_two_content">Duis hendrerit lectus non ligula cursus, et iaculis mi pharetra. Donec feugiat ultrices nibh, ut pulvinar ex feugiat in.</span><!-- sec_two_content -->
-			
+				
+				<span class="gold_link">view results</span><!-- gold_link -->
+				
 			</a>
+			
+			<div class="img_bg_wrapper">
+				
+				<img class="bg" src="<?php bloginfo('template_directory');?>/images/cr-bg-pattern-03.png"/>
+				
+			</div><!-- img_bg_wrapper -->
 			
 			
 		</div><!-- sec_two_single -->
@@ -32,9 +40,16 @@
 				<?php echo file_get_contents("wp-content/themes/begam/images/new_pattern.svg"); ?>
 				
 				<span class="sec_two_content">Since 1957, we have been helping seriously injured victims and their families recover full and fair compensation. </span><!-- sec_two_content -->
+				
+				<span class="gold_link">view results</span><!-- gold_link -->
 
-			
 			</a>
+			
+			<div class="img_bg_wrapper">
+				
+				<img class="bg" src="<?php bloginfo('template_directory');?>/images/cr-bg-pattern-01.png"/>
+				
+			</div><!-- img_bg_wrapper -->
 			
 		</div><!-- sec_two_single -->
 		
@@ -51,9 +66,17 @@
 				<?php echo file_get_contents("wp-content/themes/begam/images/new_pattern.svg"); ?>
 				
 				<span class="sec_two_content">You get the best lawyers at no charge. We only get paid if we win your case and recover compensation on your behalf.</span><!-- sec_two_content -->
+				
+				<span class="gold_link">view results</span><!-- gold_link -->
 
 			
 			</a>
+			
+			<div class="img_bg_wrapper">
+				
+				<img class="bg" src="<?php bloginfo('template_directory');?>/images/cr-new.png"/>
+				
+			</div><!-- img_bg_wrapper -->
 			
 			
 		</div><!-- sec_two_single -->
