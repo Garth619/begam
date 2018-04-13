3c53d69714ed3dc962755b5624ad80e912eec7ef
Garrett Cullen <garrettwcullen@gmail.com>
2018-04-13 16:32:56 -0700

    push

---
+++ wp-content/themes/begam/homepage_template_parts/section-4.php
@@ -0,0 +1,59 @@
+<section id="section_four">
+	
+	<span class="sec_four_case_results">Case Results</span><!-- sec_four_case_results -->
+	
+	<div class="case_results_slider">
+		
+		<div class="case_results_single_slide">
+			
+			<?php echo file_get_contents("wp-content/themes/begam/images/new_car.svg"); ?>
+			
+			<span class="case_amount">$2.5m</span><!-- case_amount -->
+			
+			<span class="case_cat">Car Accident</span><!-- case_title -->
+			
+			<div class="hover_content">
+				
+				<span>Nullam euismod massa a quam rhoncus, ac lobortis odio molestie. Vivamus tristique nunc non lectus facilisis ornare. Etiam vel venenatis orci. Sed vitae nisi sit amet mi pharetra commodo...</span>
+				
+			</div><!-- hover_content -->
+			
+		</div><!-- case_results_single_slide -->
+		
+		<div class="case_results_single_slide">
+			
+			<?php echo file_get_contents("wp-content/themes/begam/images/new_heart.svg"); ?>
+			
+			<span class="case_amount">$1.75m</span><!-- case_amount -->
+			
+			<span class="case_cat">Wrongful Death</span><!-- case_title -->
+			
+			<div class="hover_content">
+				
+				<span>Nullam euismod massa a quam rhoncus, ac lobortis odio molestie. Vivamus tristique nunc non lectus facilisis ornare. Etiam vel venenatis orci. Sed vitae nisi sit amet mi pharetra commodo...</span>
+				
+			</div><!-- hover_content -->
+			
+		</div><!-- case_results_single_slide -->
+		
+		<div class="case_results_single_slide">
+			
+			<?php echo file_get_contents("wp-content/themes/begam/images/new_steth.svg"); ?>
+			
+			<span class="case_amount">$2.5m</span><!-- case_amount -->
+			
+			<span class="case_cat">Car Accident</span><!-- case_title -->
+			
+			<div class="hover_content">
+				
+				<span>Nullam euismod massa a quam rhoncus, ac lobortis odio molestie. Vivamus tristique nunc non lectus facilisis ornare. Etiam vel venenatis orci. Sed vitae nisi sit amet mi pharetra commodo...</span>
+				
+			</div><!-- hover_content -->
+			
+		</div><!-- case_results_single_slide -->
+		
+		
+	</div><!-- case_results_slider -->
+	
+	
+</section><!-- section_four -->
