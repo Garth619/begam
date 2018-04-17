ae31a81ec0baf574a3bbc88ba2969a5750b73566
Garrett Cullen <garrettwcullen@gmail.com>
2018-04-17 15:01:31 -0700

    push

---
+++ wp-content/themes/begam/homepage_template_parts/section-6.php
@@ -0,0 +1,35 @@
+<section id="section_six">
+	
+	<div class="sec_six_inner">
+		
+		
+		<span class="sec_six_title">Client Reviews</span><!-- sec_six_title -->
+		
+		<div class="sec_six_boxes_wrapper">
+			
+			
+			<div class="reviews_wrapper">
+				
+				<div class="reviews_slider">
+					
+					
+					
+				</div><!-- reviews_slider -->
+				
+			</div><!-- reviews_wrapper -->
+			
+			<div class="reviews_img_wrapper">
+			
+				<img src="<?php bloginfo('template_directory');?>/images/reviews-desert-cactus-desktop.jpg"/>
+			
+			</div><!-- reviews_img_wrapper -->
+			
+			<div class="reviews_gold_box"></div><!-- reviews_gold_box -->
+			
+			
+		</div><!-- sec_six_boxes_wrapper -->
+		
+		
+	</div><!-- sec_six_inner -->
+	
+</section><!-- section_six -->
