06a58b9f061ea325da9ec819ba6107b22c23d30a
Garrett Cullen <garrettwcullen@gmail.com>
2018-04-25 15:36:06 -0700

    push

---
+++ wp-content/themes/begam/page-attoverview.php
@@ -0,0 +1,126 @@
+<?php 
+	
+	/* Template Name: Attorney Overview */
+	
+	get_header(); ?>
+
+
+
+<div id="main" class="internal_main">
+	
+		
+	<h1 class="no_banner_header">Attorney Overview</h1><!-- no_banner_header -->
+	
+	<div class="att_overview">
+		
+		
+		<div class="single_att_overview">
+			
+			<a class="" href="">
+			
+				<div class="single_att_inner">
+			
+					<img src="<?php bloginfo('template_directory');?>/images/att-overview-temp.jpg"/>
+			
+				</div><!-- single_att_inner -->
+			
+			</a>
+			
+			<span class="att_name">Stanley J. Marks</span><!-- att_title -->
+			
+			<span class="att_title">Attorney/Partner</span><!-- att_title -->
+			
+			
+		</div><!-- single_att_overview -->
+		
+		<div class="single_att_overview">
+			
+			<a class="" href="">
+			
+				<div class="single_att_inner">
+			
+					<img src="<?php bloginfo('template_directory');?>/images/att-overview-temp.jpg"/>
+			
+				</div><!-- single_att_inner -->
+			
+			</a>
+			
+			<span class="att_name">Stanley J. Marks</span><!-- att_title -->
+			
+			<span class="att_title">Attorney/Partner</span><!-- att_title -->
+			
+			
+		</div><!-- single_att_overview -->
+		
+		<div class="single_att_overview">
+			
+			<a class="" href="">
+			
+				<div class="single_att_inner">
+			
+					<img src="<?php bloginfo('template_directory');?>/images/att-overview-temp.jpg"/>
+			
+				</div><!-- single_att_inner -->
+			
+			</a>
+			
+			<span class="att_name">Stanley J. Marks</span><!-- att_title -->
+			
+			<span class="att_title">Attorney/Partner</span><!-- att_title -->
+			
+			
+		</div><!-- single_att_overview -->
+		
+		<div class="single_att_overview">
+			
+			<a class="" href="">
+			
+				<div class="single_att_inner">
+			
+					<img src="<?php bloginfo('template_directory');?>/images/att-overview-temp.jpg"/>
+			
+				</div><!-- single_att_inner -->
+			
+			</a>
+			
+			<span class="att_name">Stanley J. Marks</span><!-- att_title -->
+			
+			<span class="att_title">Attorney/Partner</span><!-- att_title -->
+			
+			
+		</div><!-- single_att_overview -->
+		
+		<div class="single_att_overview">
+			
+			<a class="" href="">
+			
+				<div class="single_att_inner">
+			
+					<img src="<?php bloginfo('template_directory');?>/images/att-overview-temp.jpg"/>
+			
+				</div><!-- single_att_inner -->
+			
+			</a>
+			
+			<span class="att_name">Stanley J. Marks</span><!-- att_title -->
+			
+			<span class="att_title">Attorney/Partner</span><!-- att_title -->
+			
+			
+		</div><!-- single_att_overview -->
+		
+		
+	</div><!-- att_overview -->
+
+
+</div><!-- internal_main -->
+
+	
+
+
+
+<?php get_footer(); ?>
+
+
+
+
