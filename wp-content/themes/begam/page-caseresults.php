94cd522a370c138e48c0a908f59d9eb5630e4b28
Garrett Cullen <garrettwcullen@gmail.com>
2018-04-26 16:20:01 -0700

    push

---
+++ wp-content/themes/begam/page-caseresults.php
@@ -0,0 +1,388 @@
+<?php 
+	
+	/* Template Name: Case Results */
+	
+	get_header(); ?>
+
+
+
+
+<div id="main" class="internal_main">
+	
+	<h1 class="no_banner_header"><?php the_title();?></h1><!-- no_banner_header -->
+	
+	<?php echo file_get_contents("wp-content/themes/begam/images/new_pattern.svg"); ?>
+	
+	<span class="case_results_page_intro">Our firm has recovered over $178 million for our clients</span><!-- case_results_page_intro -->
+	
+	<div class="case_results_page_wrapper">
+		
+		
+		<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_steth.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+		
+		<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_truck.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+			<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_cactus.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+			<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_fire.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+			<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_boat.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+			<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_truck.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+			<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_heart.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+			<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_motorcycle.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+			<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_steth.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+			<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_steth.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+		<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_steth.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+		<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_steth.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+		<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_steth.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+		<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_steth.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+		<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_steth.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+		<div class="case_results_page_single">
+			
+			<div class="cr_single_inner">
+			
+				<div class="svg_wrapper">
+			
+					<?php echo file_get_contents("wp-content/themes/begam/images/new_steth.svg"); ?>
+			
+				</div><!-- svg_wrapper -->
+			
+				<span class="amount">$8.9m</span><!-- amount -->
+			
+				<span class="case">Failure to Diagnose</span><!-- type -->
+			
+				<span class="type">Permanent Brain Damage</span><!-- type -->
+			
+				</div><!-- cr_single_inner -->
+			
+			<div class="cr_bg_wrapper"></div><!-- cr_bg_wrapper -->
+			
+		</div><!-- case_results_page_single -->
+		
+		
+		
+		
+		
+	</div><!-- case_results_page_wrapper -->
+	
+</div><!-- internal_main -->
+
+	
+
+
+
+<?php get_footer(); ?>
+
+
