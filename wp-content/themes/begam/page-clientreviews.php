44b0c49858e25f4c8670759a1af5220ca3750aeb
Garrett Cullen <garrettwcullen@gmail.com>
2018-04-26 09:54:42 -0700

    push

---
+++ wp-content/themes/begam/page-clientreviews.php
@@ -0,0 +1,87 @@
+<?php 
+	
+	/* Template Name: Client Reviews */
+	
+	get_header(); ?>
+
+
+
+
+<div id="main" class="internal_main">
+	
+	<h1 class="no_banner_header"><?php the_title();?></h1><!-- no_banner_header -->
+	
+	<div class="case_results_page">
+		
+		<div class="cr_col">
+			
+			<div class="cr_single">
+				
+				<span class="cr_content">Just letting you know that I appreciate the hard, good, and great job you have done. It’s a pleasure getting to know you and your staff. I appreciate every effort you’ve made to settle this matter. I’ve very glad it’s done with. Once again, thank you and appreciation to&nbsp;you.</span>
+				
+				<span class="cr_name">V.M.</span><!-- cr_name -->
+				
+			</div><!-- cr_single -->
+			
+			<div class="cr_single">
+				
+				<span class="cr_content">Ms. Chaivra: Thank you very much for all of your help in getting me through the difficult situation. Thanks for all the behind the scenes work and patience in answering all my questions. I am forever grateful.</span>
+				
+				<span class="cr_name">E.R.</span><!-- cr_name -->
+				
+			</div><!-- cr_single -->
+			
+			<div class="cr_single">
+				
+				<span class="cr_content">My grandmother and I thank you and your staff sincerely for your work on our case. These are difficult times for our family during which your caring nature and professionalism have not gone unnoticed. Once again, you have gone above and beyond the practice of law.</span>
+				
+				<span class="cr_name">A. & B.P.</span><!-- cr_name -->
+				
+			</div><!-- cr_single -->
+			
+		</div><!-- cr_col -->
+		
+		<div class="cr_col">
+			
+			
+			
+			<div class="cr_single">
+				
+				<span class="cr_content">Begam and Marks, P.A. is an excellent firm with caring, hardworking attorneys. They took a chance on my case when no one else would listen. Richard Traulsen, along with Serena Montague and Steven Friedman, came through for me and my family and secured a life changing settlement for my injuries. I could not recommend this firm highly enough or with more confidence. Thank you Begam and Marks!</span>
+				
+				<span class="cr_name">S.I.</span><!-- cr_name -->
+				
+			</div><!-- cr_single -->
+			
+			<div class="cr_single">
+				
+				<span class="cr_content">Just wanted to wish Mr. Marks, his staff and family a very happy New Year. The R/J family has always used Mr. Marks’ services in our past legal needs and he has always been successful with our different cases. Just wanted to let him know we still consider him family and he is still at the top of our Rolodex. Thank you Mr. Marks!</span>
+				
+				<span class="cr_name">W.R.</span><!-- cr_name -->
+				
+			</div><!-- cr_single -->
+			
+			<div class="cr_single">
+				
+				<span class="cr_content">Richard and Steve: You two were great to work with, and I would recommend anyone to your firm. I’m very pleased with the way everything was handled and I’m grateful for all your hard work. I wish you both all the best.</span>
+				
+				<span class="cr_name">S.I.</span><!-- cr_name -->
+				
+			</div><!-- cr_single -->
+			
+			
+			
+		</div><!-- cr_col -->
+		
+	</div><!-- case_results_page -->
+		
+	
+</div><!-- internal_main -->
+
+	
+
+
+
+<?php get_footer(); ?>
+
+
