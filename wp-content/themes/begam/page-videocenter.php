2fbdc591a57392a38715fb625a25989500fa8c73
Garrett Cullen <garrettwcullen@gmail.com>
2018-04-26 13:33:28 -0700

    push

---
+++ wp-content/themes/begam/page-videocenter.php
@@ -0,0 +1,154 @@
+<?php 
+	
+	/* Template Name: Video Center */
+	
+	get_header(); ?>
+
+
+
+<div id="main" class="internal_main">
+	
+		
+	<h1 class="no_banner_header"><?php the_title();?></h1><!-- no_banner_header -->
+	
+	<div class="video_center">
+		
+		
+		
+		<div class="single_video">
+			
+			<div class="video_thumb wistia_embed wistia_async_fvynu51wm6 popover=true popoverContent=thumbnail">
+				
+				
+				
+			</div><!-- video_thumb -->
+			
+			<div class="video_overlay">
+				
+				
+				
+			</div><!-- video_overlay -->
+			
+			<?php echo file_get_contents("wp-content/themes/begam/images/new_play.svg"); ?>
+			
+			<span class="video_title wistia_embed wistia_async_fvynu51wm6 popover=true popoverContent=html">Lorem ipsum dolor</span>
+			
+		</div><!-- single_video -->
+		
+		
+		<div class="single_video">
+			
+			<div class="video_thumb wistia_embed wistia_async_fvynu51wm6 popover=true popoverContent=thumbnail">
+				
+				
+				
+			</div><!-- video_thumb -->
+			
+			<div class="video_overlay">
+				
+				
+				
+			</div><!-- video_overlay -->
+			
+			<?php echo file_get_contents("wp-content/themes/begam/images/new_play.svg"); ?>
+			
+			<span class="video_title wistia_embed wistia_async_fvynu51wm6 popover=true popoverContent=html">Lorem ipsum dolor</span>
+			
+		</div><!-- single_video -->
+		
+		
+		<div class="single_video">
+			
+			<div class="video_thumb wistia_embed wistia_async_fvynu51wm6 popover=true popoverContent=thumbnail">
+				
+				
+				
+			</div><!-- video_thumb -->
+			
+			<div class="video_overlay">
+				
+				
+				
+			</div><!-- video_overlay -->
+			
+			<?php echo file_get_contents("wp-content/themes/begam/images/new_play.svg"); ?>
+			
+			<span class="video_title wistia_embed wistia_async_fvynu51wm6 popover=true popoverContent=html">Lorem ipsum dolor</span>
+			
+		</div><!-- single_video -->
+		
+		
+		<div class="single_video">
+			
+			<div class="video_thumb wistia_embed wistia_async_fvynu51wm6 popover=true popoverContent=thumbnail">
+				
+				
+				
+			</div><!-- video_thumb -->
+			
+			<div class="video_overlay">
+				
+				
+				
+			</div><!-- video_overlay -->
+			
+			<?php echo file_get_contents("wp-content/themes/begam/images/new_play.svg"); ?>
+			
+			<span class="video_title wistia_embed wistia_async_fvynu51wm6 popover=true popoverContent=html">Lorem ipsum dolor</span>
+			
+		</div><!-- single_video -->
+		
+		
+		<div class="single_video">
+			
+			<div class="video_thumb wistia_embed wistia_async_fvynu51wm6 popover=true popoverContent=thumbnail">
+				
+				
+				
+			</div><!-- video_thumb -->
+			
+			<div class="video_overlay">
+				
+				
+				
+			</div><!-- video_overlay -->
+			
+			<?php echo file_get_contents("wp-content/themes/begam/images/new_play.svg"); ?>
+			
+			<span class="video_title wistia_embed wistia_async_fvynu51wm6 popover=true popoverContent=html">Lorem ipsum dolor</span>
+			
+		</div><!-- single_video -->
+		
+		
+		<div class="single_video">
+			
+			<div class="video_thumb wistia_embed wistia_async_fvynu51wm6 popover=true popoverContent=thumbnail">
+				
+				
+				
+			</div><!-- video_thumb -->
+			
+			<div class="video_overlay">
+				
+				
+				
+			</div><!-- video_overlay -->
+			
+			<?php echo file_get_contents("wp-content/themes/begam/images/new_play.svg"); ?>
+			
+			<span class="video_title wistia_embed wistia_async_fvynu51wm6 popover=true popoverContent=html">Lorem ipsum dolor</span>
+			
+		</div><!-- single_video -->
+		
+		
+		
+		
+	</div><!-- video_center -->
+	
+	<script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+
+</div><!-- internal_main -->
+
+
+
+<?php get_footer(); ?>
