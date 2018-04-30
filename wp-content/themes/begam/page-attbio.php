8892557e9878407f1ea35cc1fba92d087f9e7bf7
Garrett Cullen <garrettwcullen@gmail.com>
2018-04-30 16:24:01 -0700

    push

--- wp-content/themes/begam/page-attbio.php
+++ wp-content/themes/begam/page-attbio.php
@@ -19,6 +19,8 @@
 		
 		</div><!-- att_header_center -->
 		
+		</div><!-- att_header_wrapper -->
+		
 		<div class="att_excerpt">
 			
 			<div class="att_excerpt_content">
@@ -39,12 +41,84 @@
 				
 			</div><!-- att_excerpt_content -->
 			
-			
-
+		
 		</div><!-- att_excerpt -->
 		
+		
+		<div class="att_bio_content">
+			
+			
+			<div class="att_bio_content_inner content">
+				
+				
+				<p>Attorney Stanley J. Marks practices in all areas of personal injury law and is certified by the State Bar of Arizona Board of Legal Specialization as a Specialist in Injury and Wrongful Death Litigation. He has successfully represented clients in numerous personal injury and wrongful death cases in a practice spanning more than 40 years. Mr. Marks also presides as a judge pro tem in the Maricopa County Superior Court and serves as a magistrate judge in the Town of Paradise Valley.</p>
+
+<p>Mr. Marks is a Past President of the Arizona Trial Lawyers Association, and a Past President of the Personal Injury Trial Lawyers Association. He is also a former Board Member and Officer of the Arizona Burn Foundation. He also sits on the national board of the Public Justice Foundation, an organization that fights for the rights of all Americans.</p>
+
+<p>Mr. Marks has earned an AV® Preeminent™ Peer Review Rating by Martindale-Hubbell®, the highest rating given for legal ability and ethical standards. He has also been selected as a Top Attorney in Arizona by the prestigious Southwest Super Lawyers since 2007 and is a member of the National Trial Lawyers Top 100 Trial Lawyers, Arizona. He has also been selected to Arizona’s Finest Lawyers.</p>
+				
+			</div><!-- att_bio_content_inner -->
+			
+			<div class="att_bio_sidebar">
+				
+				<div class="sidebar_col">
+					
+					<div class="single_list">
+				
+					<span class="list_title">Admissions</span><!-- list_title -->
+					
+					<ul>
+						<li>Arizona, 1996</li>
+						<li>U.S. Supreme Court, 1973</li>
+					</ul>
+					
+					</div><!-- single_list -->
+					
+					<div class="single_list">
+				
+					<span class="list_title">Professional Associations</span><!-- list_title -->
+					
+					<ul>
+						<li>LL.B., New York University, 1965<i><br/>American Jurisprudence Awards for Procedure and Torts</i></li>
+						<li>U.S. Supreme Court, 1973</li>
+					</ul>
+					
+					</div><!-- single_list -->
+				
+				</div><!-- sidebar_col -->
+				
+				<div class="sidebar_col">
+					
+					<div class="single_list">
+				
+					<span class="list_title">Education</span><!-- list_title -->
+					
+					<ul>
+						<li>LL.B., New York University, 1965<i><br/>American Jurisprudence Awards for Procedure and Torts</i></li>
+						<li>U.S. Supreme Court, 1973</li>
+					</ul>
+					
+					</div><!-- single_list -->
+					
+					<div class="single_list">
+				
+					<span class="list_title">Volunteer Experience</span><!-- list_title -->
+					
+					<ul>
+						<li>LL.B., New York University, 1965<i><br/>American Jurisprudence Awards for Procedure and Torts</i></li>
+						<li>U.S. Supreme Court, 1973</li>
+					</ul>
+					
+					</div><!-- single_list -->
+				
+				</div><!-- sidebar_col -->
+				
+			</div><!-- att_bio_sidebar -->
+			
+		</div><!-- att_bio_content -->
+		
 	
-	</div><!-- att_header_wrapper -->
+	
 	
 </div><!-- internal_main -->
 
